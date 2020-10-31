d96c493d3d0060577e0549e28f3981c1768d170c
Gnik Droy <gnikdroy@gmail.com>
2020-10-31 13:09:20 +0545

    Add window events.

--- include/events/window.hpp
+++ include/events/window.hpp
@@ -1,1 +1,44 @@
 #pragma once
+
+namespace k2 {
+class Window;
+
+struct WindowCloseEvent {
+    Window* window;
+};
+
+struct WindowResizeEvent {
+    Window* window;
+    int width, height;
+};
+
+struct WindowFamebufferResizeEvent {
+    Window* window;
+    int width, height;
+};
+
+struct WindowContentScaleChangeEvent {
+    Window* window;
+    float x, y;
+};
+
+struct WindowRepositionEvent {
+    Window* window;
+    int x, y;
+};
+
+struct WindowIconifyEvent {
+    Window* window;
+    bool iconified;
+};
+
+struct WindowMaximizeEvent {
+    Window* window;
+    bool maximized;
+};
+
+struct WindowFocusChangeEvent {
+    Window* window;
+    bool focused;
+};
+}  // namespace k2
