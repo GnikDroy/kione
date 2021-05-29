6310775d62b077c289c9de57d6d8e24cb610f70a
Gnik Droy <gnikdroy@gmail.com>
2021-05-29 15:30:52 +0545

    Simple RAII wrapper for Image (stb)

---
+++ include/core/rendering/image.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include "stb_image.h"
+#include <filesystem>
+
+namespace k2 {
+    struct Image {
+        int width{};
+        int height{};
+        int channels{};
+        stbi_uc* data{};
+
+        explicit Image(const std::filesystem::path& path, int desired_channels = 0) {
+            stbi_set_flip_vertically_on_load(true);
+            data = stbi_load(path.generic_string().c_str(), &width, &height, &channels, desired_channels);
+        }
+
+        static std::string_view error_msg(){
+            return stbi_failure_reason();
+        }
+
+        operator bool() const { return data != nullptr; }
+
+        ~Image() {
+            stbi_image_free(data);
+        }
+    };
+}
