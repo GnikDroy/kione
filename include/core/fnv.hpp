15def64c06816ec3259dc9db3024f10f6983368b
Gnik Droy <gnikdroy@gmail.com>
2021-05-30 18:53:20 +0545

    src: add free hash functions

--- include/core/fnv.hpp
+++ include/core/fnv.hpp
@@ -2,8 +2,8 @@
 
 #include <cstdint>
 
-namespace k2::literals {
-    constexpr std::uint64_t operator ""_fnv1a(const char *str, std::size_t len) {
+namespace k2 {
+    constexpr std::uint64_t fnv1a(const char * str, std::size_t len) {
         std::uint64_t prime = 1099511628211;
         std::uint64_t hash = 14695981039346656037ull;
         for (std::size_t i = 0; i < len; i++) {
@@ -12,5 +12,14 @@ namespace k2::literals {
         }
         return hash;
     }
-}
 
+    constexpr std::uint64_t fnv1a(const std::string& str) {
+        return fnv1a(str.c_str(), str.size());
+    }
+
+    namespace literals {
+        constexpr std::uint64_t operator ""_fnv1a(const char *str, std::size_t len) {
+            return fnv1a(str, len);
+        }
+    }
+}
