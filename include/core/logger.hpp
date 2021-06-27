d0dfdbf299c5a666bb23eeabc88cda4f4e33a3f0
Gnik Droy <gnikdroy@gmail.com>
2021-06-27 18:14:35 +0545

    src: add ring buffer sink for logger.

--- include/core/logger.hpp
+++ include/core/logger.hpp
@@ -1,9 +1,20 @@
 #pragma once
+#include <any>
 #include <memory>
 #include <string>
 
 namespace k2 {
 namespace Log {
+    enum class LogLevel {
+        Trace,
+        Debug,
+        Info,
+        Warn,
+        Err,
+        Critical,
+        Off,
+    };
+
     class Logger {
         struct Impl;
         std::unique_ptr<Impl> impl;
@@ -13,11 +24,29 @@ namespace Log {
         Logger(const Logger&) = delete;
         Logger& operator=(const Logger&) = delete;
 
-        void trace(const std::string& msg);
-        void info(const std::string& msg);
-        void warn(const std::string& msg);
-        void error(const std::string& msg);
-        void critical(const std::string& msg);
+        Logger& add_sink(std::any& sink_handle);
+
+        Logger& set_level(LogLevel level);
+        Logger& trace(const std::string& msg);
+        Logger& info(const std::string& msg);
+        Logger& warn(const std::string& msg);
+        Logger& error(const std::string& msg);
+        Logger& critical(const std::string& msg);
+    };
+
+    enum class LoggerSinkType {
+        SingleThreaded,
+        MultiThreaded,
+    };
+
+    template <LoggerSinkType T> class RingBufferSink {
+        std::any impl;
+
+    public:
+        explicit RingBufferSink(size_t num_items = 0);
+        std::any& get_sink();
+        void set_pattern(const std::string& pattern);
+        std::vector<std::string> get(size_t num_items = 0);
     };
 
     Logger& app();
--- src/core/logger.cpp
+++ src/core/logger.cpp
@@ -1,4 +1,8 @@
 #include "core/logger.hpp"
+#include <spdlog/sinks/ringbuffer_sink.h>
+#include <spdlog/spdlog.h>
+
+#include <any>
 
 namespace k2::Log {
 struct Logger::Impl {
@@ -14,17 +18,55 @@ struct Logger::Impl {
         logger->set_level(spdlog::level::trace);
         logger->flush_on(spdlog::level::trace);
     }
+
+    void register_logger(std::any& sink_handle) {
+        logger->sinks().push_back(std::any_cast<spdlog::sink_ptr>(sink_handle));
+    }
     std::shared_ptr<spdlog::logger> logger;
 };
 
 Logger::Logger(const std::string& name)
     : impl(std::move(std::make_unique<Logger::Impl>(name))) { }
 
-void Logger::trace(const std::string& message) { impl->logger->trace(message); }
-void Logger::info(const std::string& message) { impl->logger->info(message); }
-void Logger::warn(const std::string& message) { impl->logger->warn(message); }
-void Logger::error(const std::string& message) { impl->logger->error(message); }
-void Logger::critical(const std::string& message) { impl->logger->critical(message); }
+Logger& Logger::add_sink(std::any& sink_handle) {
+    impl->register_logger(sink_handle);
+    return *this;
+}
+
+Logger& Logger::set_level(LogLevel level) {
+    switch (level) {
+    case LogLevel::Trace: impl->logger->set_level(spdlog::level::trace); break;
+    case LogLevel::Debug: impl->logger->set_level(spdlog::level::debug); break;
+    case LogLevel::Info: impl->logger->set_level(spdlog::level::info); break;
+    case LogLevel::Warn: impl->logger->set_level(spdlog::level::warn); break;
+    case LogLevel::Err: impl->logger->set_level(spdlog::level::err); break;
+    case LogLevel::Critical: impl->logger->set_level(spdlog::level::critical); break;
+    case LogLevel::Off:
+    default: impl->logger->set_level(spdlog::level::off);
+    }
+    return *this;
+}
+
+Logger& Logger::trace(const std::string& message) {
+    impl->logger->trace(message);
+    return *this;
+}
+Logger& Logger::info(const std::string& message) {
+    impl->logger->info(message);
+    return *this;
+}
+Logger& Logger::warn(const std::string& message) {
+    impl->logger->warn(message);
+    return *this;
+}
+Logger& Logger::error(const std::string& message) {
+    impl->logger->error(message);
+    return *this;
+}
+Logger& Logger::critical(const std::string& message) {
+    impl->logger->critical(message);
+    return *this;
+}
 
 Logger& app() {
     static Logger logger { "APP" };
@@ -35,4 +77,32 @@ Logger& core() {
     static Logger logger { "CORE" };
     return logger;
 }
+
+template <auto V> struct LoggerInnerType;
+
+template <auto V> using LoggerInnerTypeV = typename LoggerInnerType<V>::type;
+
+template <> struct LoggerInnerType<LoggerSinkType::SingleThreaded> { using type = spdlog::details::null_mutex; };
+
+template <> struct LoggerInnerType<LoggerSinkType::MultiThreaded> { using type = std::mutex; };
+
+template <LoggerSinkType V> using RingBufferSinkT = spdlog::sinks::ringbuffer_sink<LoggerInnerTypeV<V>>;
+
+template <LoggerSinkType V>
+RingBufferSink<V>::RingBufferSink(size_t num_items)
+    : impl { std::static_pointer_cast<spdlog::sinks::sink>(std::make_shared<RingBufferSinkT<V>>(num_items)) } { }
+
+template <LoggerSinkType T> std::any& RingBufferSink<T>::get_sink() { return impl; }
+
+template <LoggerSinkType V> std::vector<std::string> RingBufferSink<V>::get(size_t num_items) {
+    return std::reinterpret_pointer_cast<RingBufferSinkT<V>>(std::any_cast<spdlog::sink_ptr>(impl))
+        ->last_formatted(num_items);
+}
+template <LoggerSinkType V> void RingBufferSink<V>::set_pattern(const std::string& pattern) {
+    return std::reinterpret_pointer_cast<RingBufferSinkT<V>>(std::any_cast<spdlog::sink_ptr>(impl))
+        ->set_pattern(pattern);
+}
+
+template class RingBufferSink<LoggerSinkType::SingleThreaded>;
+template class RingBufferSink<LoggerSinkType::MultiThreaded>;
 }
