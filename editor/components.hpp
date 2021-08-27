3807ff7d2f57b1478d1b8964a58d0380d64af103
Gnik Droy <gnikdroy@gmail.com>
2021-08-27 11:33:11 +0545

    editor: inspector UI for added components.

--- editor/components.hpp
+++ editor/components.hpp
@@ -2,49 +2,97 @@
 #include <entt/entt.hpp>
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/matrix_decompose.hpp>
+#include <glm/gtx/quaternion.hpp>
 #include <imgui.h>
+#include <imgui_internal.h>
 
-struct Transform {
-    glm::vec3 position;
-    glm::vec3 scale { 1.0f };
-    glm::vec3 rotation;
-};
-
-struct DirectionalLight {
-    glm::vec3 direction;
-
-    glm::vec3 ambient;
-    glm::vec3 diffuse;
-    glm::vec3 specular;
-};
-
-struct PointLight {
-    glm::vec3 ambient;
-    glm::vec3 diffuse;
-    glm::vec3 specular;
-};
+#include "components/camera.hpp"
+#include "components/relation.hpp"
+#include "components/sprite.hpp"
+#include "components/tag.hpp"
+#include "components/transform.hpp"
+#include "ui/common.hpp"
 
 namespace k2 {
-template <> void ComponentInspectorWidget<Transform>(entt::registry& reg, entt::registry::entity_type e) {
-    auto& transform = reg.get<Transform>(e);
-    ImGui::DragFloat3("Position", glm::value_ptr(transform.position), 0.2f);
-    ImGui::DragFloat3("Rotation", glm::value_ptr(transform.rotation), 0.2f);
-    ImGui::DragFloat3("Scale", glm::value_ptr(transform.scale), 0.2f);
+template <> void ComponentInspectorWidget<k2::TransformComponent>(entt::registry& reg, entt::registry::entity_type e) {
+    auto& transform = reg.get<TransformComponent>(e);
+    k2::editor::Vec3InputWidget("Translation", transform.translation);
+    k2::editor::Vec3InputWidget("Scale", transform.scale);
+    k2::editor::OrientationInputWidget("Orientation", transform.orientation);
 }
 
-template <> void ComponentInspectorWidget<PointLight>(entt::registry& reg, entt::registry::entity_type e) {
-    auto& light = reg.get<PointLight>(e);
-    ImGui::DragFloat3("Ambient", glm::value_ptr(light.ambient), 0.05f);
-    ImGui::DragFloat3("Diffuse", glm::value_ptr(light.diffuse), 0.05f);
-    ImGui::DragFloat3("Specular", glm::value_ptr(light.specular), 0.05f);
+template <> void ComponentInspectorWidget<k2::Camera>(entt::registry& reg, entt::registry::entity_type e) {
+    auto& camera = reg.get<k2::Camera>(e);
+    k2::editor::Vec3InputWidget("Position", camera.position);
+    k2::editor::Vec3InputWidget("Target", camera.target);
+    k2::editor::Vec3InputWidget("Up", camera.up);
+
+    constexpr auto items = []() constexpr {
+        std::array<const char*, 2> items {};
+        items[int(k2::Camera::Projection::Perspective)] = "Perspective";
+        items[int(k2::Camera::Projection::Orthographic)] = "Orthographic";
+        return items;
+    }
+    ();
+    int item = int(camera.projection_traits.index());
+    ImGui::Combo("Projection Type", &item, items.data(), int(items.size()));
+
+    // Switch to the proper variant according to the combo item if different.
+    auto projection_type = k2::Camera::Projection(item);
+    switch (projection_type) {
+    case k2::Camera::Projection::Perspective: {
+        if (!std::holds_alternative<k2::Camera::ProjectionTraits<k2::Camera::Projection::Perspective>>(
+                camera.projection_traits)) {
+            camera.projection_traits = k2::Camera::ProjectionTraits<k2::Camera::Projection::Perspective> {};
+        }
+        break;
+    }
+    case k2::Camera::Projection::Orthographic: {
+        if (!std::holds_alternative<k2::Camera::ProjectionTraits<k2::Camera::Projection::Orthographic>>(
+                camera.projection_traits)) {
+            camera.projection_traits = k2::Camera::ProjectionTraits<k2::Camera::Projection::Orthographic> {};
+        }
+        break;
+    }
+    default: assert(false && "Invalid projection type.");
+    }
+
+    std::visit(
+        [](auto&& traits) {
+            using T = std::decay_t<decltype(traits)>;
+            if constexpr (std::is_same_v<T, k2::Camera::ProjectionTraits<k2::Camera::Projection::Perspective>>) {
+                ImGui::InputFloat("FOV", &traits.fov);
+                ImGui::InputFloat("Aspect Ratio", &traits.aspect_ratio);
+                ImGui::InputFloat("Near Clip", &traits.near_clip);
+                ImGui::InputFloat("Far Clip", &traits.far_clip);
+
+            } else if constexpr (std::is_same_v<T,
+                                     k2::Camera::ProjectionTraits<k2::Camera::Projection::Orthographic>>) {
+                ImGui::InputFloat("Left", &traits.left);
+                ImGui::InputFloat("Right", &traits.right);
+                ImGui::InputFloat("Bottom", &traits.bottom);
+                ImGui::InputFloat("Top", &traits.top);
+                ImGui::InputFloat("Near Clip", &traits.near_clip);
+                ImGui::InputFloat("Far Clip", &traits.far_clip);
+            } else {
+                static_assert(always_false<T>, "Invalid variant!");
+            }
+        },
+        camera.projection_traits);
 }
 
-template <> void ComponentInspectorWidget<DirectionalLight>(entt::registry& reg, entt::registry::entity_type e) {
-    auto& light = reg.get<DirectionalLight>(e);
-    ImGui::DragFloat3("Direction", glm::value_ptr(light.direction), 0.05f);
-    ImGui::DragFloat3("Ambient", glm::value_ptr(light.ambient), 0.05f);
-    ImGui::DragFloat3("Diffuse", glm::value_ptr(light.diffuse), 0.05f);
-    ImGui::DragFloat3("Specular", glm::value_ptr(light.specular), 0.05f);
+template <> void ComponentInspectorWidget<k2::SpriteComponent>(entt::registry& reg, entt::registry::entity_type e) {
+    auto& sprite = reg.get<k2::SpriteComponent>(e);
+    ImGui::ColorEdit4("Color", glm::value_ptr(sprite.color));
+    std::array<char, 50> input;
+    ImGui::InputText("Resource ID", input.data(), input.size());
+
+    k2::editor::RectInputWidget("UV Rect", sprite.uv_rect);
 }
 
+template <> void ComponentInspectorWidget<k2::TagComponent>(entt::registry& reg, entt::registry::entity_type e) {
+    auto& tag = reg.get<k2::TagComponent>(e);
+    ImGui::InputText("Tag", tag.tag.data(), tag.tag.size());
+}
 }
---
+++ editor/ui/common.hpp
@@ -0,0 +1,263 @@
+#pragma once
+#include "core/utils.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/matrix_decompose.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include <imgui.h>
+#include <imgui_internal.h>
+
+namespace k2::editor {
+
+template <k2::arithmetic T, class... Args> constexpr auto ImGuiDrag(Args&&... args) {
+    if constexpr (std::is_same_v<T, int>) {
+        return ImGui::DragInt(std::forward<Args>(args)...);
+    } else if constexpr (std::is_same_v<T, float>) {
+        return ImGui::DragFloat(std::forward<Args>(args)...);
+    } else {
+        static_assert(k2::always_false<T>, "Arithmetic type not implemented.");
+    }
+}
+
+template <class T>
+void Vec3InputWidget(
+    const std::string& label, glm::vec<3, T, glm::defaultp>& vec, const glm::vec<3, T, glm::defaultp>& reset = {}) {
+    constexpr auto num_columns = 2;
+    if (ImGui::BeginTable(label.c_str(), num_columns)) {
+        ImGui::TableSetupColumn("###Items", ImGuiTableColumnFlags_WidthStretch, 3);
+        ImGui::TableSetupColumn("###Labels", ImGuiTableColumnFlags_WidthStretch, 1);
+        ImGui::TableNextColumn();
+        ImGui::PushMultiItemsWidths(3, ImGui::CalcItemWidth());
+        ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2 { 0, 0 });
+
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4 { 0.8f, 0.1f, 0.10f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4 { 0.9f, 0.2f, 0.2f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4 { 0.8f, 0.1f, 0.10f, 1.0f });
+        if (ImGui::Button("X")) {
+            vec.x = reset.x;
+        }
+        ImGui::PopStyleColor(3);
+
+        ImGui::SameLine();
+        ImGuiDrag<T>("##X", &vec.x, 0.1f);
+        ImGui::SameLine();
+        ImGui::PopItemWidth();
+
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4 { 0.2f, 0.5f, 0.10f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4 { 0.2f, 0.6f, 0.2f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4 { 0.2f, 0.5f, 0.10f, 1.0f });
+        if (ImGui::Button("Y")) {
+            vec.y = reset.y;
+        }
+        ImGui::PopStyleColor(3);
+
+        ImGui::SameLine();
+        ImGuiDrag<T>("##Y", &vec.y, 0.1f);
+        ImGui::SameLine();
+        ImGui::PopItemWidth();
+
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4 { 0.1f, 0.1f, 0.8f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4 { 0.2f, 0.2f, 0.9f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4 { 0.1f, 0.1f, 0.8f, 1.0f });
+        if (ImGui::Button("Z")) {
+            vec.z = reset.z;
+        }
+        ImGui::PopStyleColor(3);
+        ImGui::SameLine();
+        ImGuiDrag<T>("##Z", &vec.z, 0.1f);
+        ImGui::SameLine();
+        ImGui::PopItemWidth();
+        ImGui::PopStyleVar();
+
+        ImGui::TableNextColumn();
+        ImGui::TextUnformatted(label.c_str());
+        ImGui::EndTable();
+    }
+}
+
+template <class T>
+void Vec2InputWidget(const std::string& label, glm::vec<2, T, glm::defaultp>& vec,
+    const glm::vec<2, T, glm::defaultp>& reset = { .0f }) {
+    constexpr auto num_columns = 2;
+    if (ImGui::BeginTable(label.c_str(), num_columns)) {
+        ImGui::TableSetupColumn("###Items", ImGuiTableColumnFlags_WidthStretch, 3);
+        ImGui::TableSetupColumn("###Labels", ImGuiTableColumnFlags_WidthStretch, 1);
+        ImGui::TableNextColumn();
+        ImGui::PushMultiItemsWidths(2, ImGui::CalcItemWidth());
+        ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2 { 0, 0 });
+
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4 { 0.8f, 0.1f, 0.10f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4 { 0.9f, 0.2f, 0.2f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4 { 0.8f, 0.1f, 0.10f, 1.0f });
+        if (ImGui::Button("X")) {
+            vec.x = reset.x;
+        }
+        ImGui::PopStyleColor(3);
+
+        ImGui::SameLine();
+        ImGuiDrag<T>("##X", &vec.x, 0.1f);
+        ImGui::SameLine();
+        ImGui::PopItemWidth();
+
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4 { 0.2f, 0.5f, 0.10f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4 { 0.2f, 0.6f, 0.2f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4 { 0.2f, 0.5f, 0.10f, 1.0f });
+        if (ImGui::Button("Y")) {
+            vec.y = reset.y;
+        }
+        ImGui::PopStyleColor(3);
+
+        ImGui::SameLine();
+        ImGuiDrag<T>("##Y", &vec.y, 0.1f);
+        ImGui::SameLine();
+        ImGui::PopItemWidth();
+
+        ImGui::PopStyleVar();
+
+        ImGui::TableNextColumn();
+        ImGui::TextUnformatted(label.c_str());
+        ImGui::EndTable();
+    }
+}
+
+template <class T>
+void Vec4InputWidget(const std::string& label, glm::vec<3, T, glm::defaultp>& vec,
+    const glm::vec<3, T, glm::defaultp>& reset = { .0f }) {
+    constexpr auto num_columns = 2;
+    if (ImGui::BeginTable(label.c_str(), num_columns)) {
+        ImGui::TableSetupColumn("###Items", ImGuiTableColumnFlags_WidthStretch, 3);
+        ImGui::TableSetupColumn("###Labels", ImGuiTableColumnFlags_WidthStretch, 1);
+        ImGui::TableNextColumn();
+        ImGui::PushMultiItemsWidths(4, ImGui::CalcItemWidth());
+        ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2 { 0, 0 });
+
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4 { 0.8f, 0.1f, 0.10f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4 { 0.9f, 0.2f, 0.2f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4 { 0.8f, 0.1f, 0.10f, 1.0f });
+        if (ImGui::Button("X")) {
+            vec.x = reset.x;
+        }
+        ImGui::PopStyleColor(3);
+
+        ImGui::SameLine();
+        ImGuiDrag<T>("##X", &vec.x, 0.1f);
+        ImGui::SameLine();
+        ImGui::PopItemWidth();
+
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4 { 0.2f, 0.5f, 0.10f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4 { 0.2f, 0.6f, 0.2f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4 { 0.2f, 0.5f, 0.10f, 1.0f });
+        if (ImGui::Button("Y")) {
+            vec.y = reset.y;
+        }
+        ImGui::PopStyleColor(3);
+
+        ImGui::SameLine();
+        ImGuiDrag<T>("##Y", &vec.y, 0.1f);
+        ImGui::SameLine();
+        ImGui::PopItemWidth();
+
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4 { 0.1f, 0.1f, 0.8f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4 { 0.2f, 0.2f, 0.9f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4 { 0.1f, 0.1f, 0.8f, 1.0f });
+        if (ImGui::Button("Z")) {
+            vec.z = reset.z;
+        }
+        ImGui::PopStyleColor(3);
+        ImGui::SameLine();
+        ImGuiDrag<T>("##Z", &vec.z, 0.1f);
+        ImGui::SameLine();
+        ImGui::PopItemWidth();
+
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4 { 0.5f, 0.3f, 0.6f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4 { 0.6f, 0.4f, 0.7f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4 { 0.5f, 0.3f, 0.6f, 1.0f });
+        if (ImGui::Button("W")) {
+            vec.w = reset.w;
+        }
+        ImGui::PopStyleColor(3);
+        ImGui::SameLine();
+        ImGuiDrag<T>("##W", &vec.w, 0.1f);
+        ImGui::SameLine();
+        ImGui::PopItemWidth();
+        ImGui::PopStyleVar();
+
+        ImGui::TableNextColumn();
+        ImGui::TextUnformatted(label.c_str());
+        ImGui::EndTable();
+    }
+}
+template <arithmetic T>
+void RectInputWidget(const std::string& label, k2::Rect<T>& rect, const k2::Rect<T>& reset = {}) {
+    constexpr auto num_columns = 2;
+    if (ImGui::BeginTable(label.c_str(), num_columns)) {
+        ImGui::TableSetupColumn("###Items", ImGuiTableColumnFlags_WidthStretch, 3);
+        ImGui::TableSetupColumn("###Labels", ImGuiTableColumnFlags_WidthStretch, 1);
+        ImGui::TableNextColumn();
+        ImGui::PushMultiItemsWidths(4, ImGui::CalcItemWidth());
+        ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2 { 0, 0 });
+
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4 { 0.8f, 0.1f, 0.10f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4 { 0.9f, 0.2f, 0.2f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4 { 0.8f, 0.1f, 0.10f, 1.0f });
+        if (ImGui::Button("X")) {
+            rect.x = reset.x;
+        }
+        ImGui::PopStyleColor(3);
+
+        ImGui::SameLine();
+        ImGui::DragFloat("##X", &rect.x, 0.1f);
+        ImGui::SameLine();
+        ImGui::PopItemWidth();
+
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4 { 0.2f, 0.5f, 0.10f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4 { 0.2f, 0.6f, 0.2f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4 { 0.2f, 0.5f, 0.10f, 1.0f });
+        if (ImGui::Button("Y")) {
+            rect.y = reset.y;
+        }
+        ImGui::PopStyleColor(3);
+
+        ImGui::SameLine();
+        ImGui::DragFloat("##Y", &rect.y, 0.1f);
+        ImGui::SameLine();
+        ImGui::PopItemWidth();
+
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4 { 0.1f, 0.1f, 0.8f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4 { 0.2f, 0.2f, 0.9f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4 { 0.1f, 0.1f, 0.8f, 1.0f });
+        if (ImGui::Button("W")) {
+            rect.w = reset.w;
+        }
+        ImGui::PopStyleColor(3);
+        ImGui::SameLine();
+        ImGui::DragFloat("##W", &rect.w, 0.1f);
+        ImGui::SameLine();
+        ImGui::PopItemWidth();
+
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4 { 0.5f, 0.3f, 0.6f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4 { 0.6f, 0.4f, 0.7f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4 { 0.5f, 0.3f, 0.6f, 1.0f });
+        if (ImGui::Button("H")) {
+            rect.h = reset.h;
+        }
+        ImGui::PopStyleColor(3);
+        ImGui::SameLine();
+        ImGui::DragFloat("##H", &rect.h, 0.1f);
+        ImGui::SameLine();
+        ImGui::PopItemWidth();
+        ImGui::PopStyleVar();
+
+        ImGui::TableNextColumn();
+        ImGui::TextUnformatted(label.c_str());
+        ImGui::EndTable();
+    }
+}
+
+void OrientationInputWidget(const std::string& label, glm::quat& quat) {
+    auto euler_angles = glm::degrees(glm::eulerAngles(quat));
+    Vec3InputWidget(label.c_str(), euler_angles);
+    euler_angles = glm::radians(euler_angles);
+    quat = glm::quat(euler_angles);
+}
+}
