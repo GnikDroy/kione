01aea6d081739b31491bd3fe357764402391664a
Gnik Droy <gnikdroy@gmail.com>
2021-08-27 11:28:55 +0545

    src: applications prefer high performance GPU on windows.

--- include/platform/entry_point.hpp
+++ include/platform/entry_point.hpp
@@ -7,6 +7,12 @@ extern std::unique_ptr<k2::App> create_app();
 #ifdef __linux__
 int main(int, char**, char**) { create_app()->run(); }
 #elif _WIN32
+// Prefer the use of the high performance GPU
+extern "C" {
+__declspec(dllexport) uint32_t NvOptimusEnablement = 1;
+__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+
 int __stdcall WinMain(void*, void*, char*, int) {
     create_app()->run();
     return 0;
