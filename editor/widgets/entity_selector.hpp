97577cdbae76397044e7a5d689b6c00d0f64b9a1
Gnik Droy <gnikdroy@gmail.com>
2021-08-27 11:34:05 +0545

    editor: entity selector uses tag. Can create/delete entities.

--- editor/widgets/entity_selector.hpp
+++ editor/widgets/entity_selector.hpp
@@ -10,6 +10,8 @@
 #include <entt/entt.hpp>
 #include <imgui.h>
 
+#include "components/tag.hpp"
+
 namespace k2 {
 
 template <class EntityType> class EntitySelector {
@@ -30,17 +32,38 @@ public:
             if (entity == active_entity)
                 flags |= ImGuiTreeNodeFlags_Selected;
 
-            bool node_open = ImGui::TreeNodeEx(reinterpret_cast<const void*>((uintptr_t)entt::to_integral(entity)),
-                flags, "Selectable Node for entity %d", entt::to_integral(entity));
+            bool node_open {};
+            auto* tag = registry.try_get<TagComponent>(entity);
+            if (tag && !tag->str().empty()) {
+                node_open = ImGui::TreeNodeEx(
+                    reinterpret_cast<const void*>((uintptr_t)entt::to_integral(entity)), flags, "%s", tag->tag.data());
+            } else {
+                node_open = ImGui::TreeNodeEx(reinterpret_cast<const void*>((uintptr_t)entt::to_integral(entity)),
+                    flags, "Entity %d", entt::to_integral(entity));
+            }
             if (ImGui::IsItemClicked())
                 entity_clicked = entity;
+
+            if (ImGui::BeginPopupContextItem()) {
+                if (ImGui::MenuItem("Delete"))
+                    registry.destroy(entity);
+                ImGui::EndPopup();
+            }
+
             if (node_open) {
                 ImGui::BulletText("This node is open");
                 ImGui::TreePop();
             }
         });
-        if (entity_clicked != active_entity) {
-            active_entity = entity_clicked;
+        active_entity = entity_clicked;
+
+        // Add new entity
+        if (ImGui::BeginPopupContextWindow(
+                "##CreateEntityPopup", ImGuiPopupFlags_NoOpenOverItems | ImGuiPopupFlags_MouseButtonRight)) {
+            if (ImGui::MenuItem("Create Entity")) {
+                static_cast<void>(registry.create());
+            }
+            ImGui::EndPopup();
         }
     }
 
