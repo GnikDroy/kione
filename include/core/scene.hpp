cfadd0aac717fb1504554d790b9ae04f4bcc1ecc
Gnik Droy <gnikdroy@gmail.com>
2021-08-29 19:29:23 +0545

    src: add move constructor to scene

--- include/core/scene.hpp
+++ include/core/scene.hpp
@@ -6,5 +6,13 @@
 namespace k2 {
 struct Scene {
     entt::registry registry;
+
+    Scene() = default;
+    Scene(Scene&& scene)
+        : registry { std::move(scene.registry) } { }
+    Scene& operator=(Scene&& scene) {
+        std::swap(registry, scene.registry);
+        return *this;
+    }
 };
 }
