837a76fc0d86fc1ce8c1a74cfc67631ed838dc03
Gnik Droy <gnikdroy@gmail.com>
2021-08-29 19:29:08 +0545

    src: fix serializer bugs

--- include/serializers/components/transform.hpp
+++ include/serializers/components/transform.hpp
@@ -4,7 +4,7 @@
 #include <cereal/cereal.hpp>
 
 namespace cereal {
-template <class Archive> void serialize(Archive& ar, k2::TransformComponent t) {
+template <class Archive> void serialize(Archive& ar, k2::TransformComponent& t) {
     ar(make_nvp("Translation", t.translation));
     ar(make_nvp("Orientation", t.orientation));
     ar(make_nvp("Scale", t.scale));
--- include/serializers/utils.hpp
+++ include/serializers/utils.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "core/utils.hpp"
 #include <array>
+#include <cereal/archives/json.hpp>
 #include <cereal/cereal.hpp>
 #include <cereal/types/array.hpp>
 #include <glm/glm.hpp>
@@ -8,22 +9,30 @@
 
 namespace cereal {
 template <class Archive> void serialize(Archive& ar, glm::vec3& vec) {
-    ar(cereal::make_size_tag(3));
+    if constexpr (std::is_same_v<Archive, cereal::JSONOutputArchive>) {
+        ar(cereal::make_size_tag(3));
+    }
     ar(vec.x, vec.y, vec.z);
 }
 template <class Archive> void serialize(Archive& ar, glm::vec4& vec) {
-    ar(cereal::make_size_tag(4));
+    if constexpr (std::is_same_v<Archive, cereal::JSONOutputArchive>) {
+        ar(cereal::make_size_tag(4));
+    }
     ar(vec.x, vec.y, vec.z, vec.w);
 }
 
 template <class Archive> void serialize(Archive& ar, glm::quat& quaternion) {
-    ar(cereal::make_size_tag(4));
+    if constexpr (std::is_same_v<Archive, cereal::JSONOutputArchive>) {
+        ar(cereal::make_size_tag(4));
+    }
     ar(quaternion.x, quaternion.y, quaternion.z, quaternion.w);
 }
 
 template <class Archive> void serialize(Archive& ar, glm::mat4& mat) {
     auto* ptr = glm::value_ptr(mat);
-    ar(cereal::make_size_tag(4 * 4));
+    if constexpr (std::is_same_v<Archive, cereal::JSONOutputArchive>) {
+        ar(cereal::make_size_tag(4 * 4));
+    }
     for (auto i = 0; i < 4 * 4; i++) {
         ar(ptr[i]);
     }
