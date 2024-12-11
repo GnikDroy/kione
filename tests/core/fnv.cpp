e58d3b20fb366d60fb86ceb2a04f56b4779311d0
Gnik Droy <gnikdroy@gmail.com>
2024-12-11 03:41:51 -0600

    tests, build: update catch2

--- tests/core/fnv.cpp
+++ tests/core/fnv.cpp
@@ -1,4 +1,4 @@
-#include <catch2/catch.hpp>
+#include <catch2/catch_all.hpp>
 
 #include "core/fnv.hpp"
 
@@ -12,13 +12,13 @@ TEST_CASE("FNV hash works") {
     // String hash
     {
         const std::string kione { "kione" };
-        REQUIRE(k2::fnv1a(kione) == 0xcbf29ce484222325);
-        REQUIRE(k2::fnv1a(kione.data(), kione.size()) == 0xcbf29ce484222325);
+        REQUIRE(k2::fnv1a(kione) == 0x86a6488dbe53d0f3);
+        REQUIRE(k2::fnv1a(kione.data(), kione.size()) == 0x86a6488dbe53d0f3);
     }
 
     // Literals
     {
         using namespace k2::literals;
-        REQUIRE("kione"_fnv1a == 0xcbf29ce484222325);
+        REQUIRE("kione"_fnv1a == 0x86a6488dbe53d0f3);
     }
 }
