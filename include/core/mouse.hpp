c7d3450afac25762e8696c7dd7ede9b238bf2fb7
Gnik Droy <gnikdroy@gmail.com>
2024-12-11 03:19:16 -0600

    build: remove conflict with X11 macro

--- include/core/mouse.hpp
+++ include/core/mouse.hpp
@@ -8,19 +8,19 @@ class Window;
 class MouseDevice : public IInputDevice {
 public:
     enum class ButtonCode {
-        Button0 = 0,
-        Button1 = 1,
-        Button2 = 2,
-        Button3 = 3,
-        Button4 = 4,
-        Button5 = 5,
-        Button6 = 6,
-        Button7 = 7,
+        Zero = 0,
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
 
-        ButtonLast = Button7,
-        ButtonLeft = Button0,
-        ButtonRight = Button1,
-        ButtonMiddle = Button2
+        Last = Seven,
+        Left = Zero,
+        Right = One,
+        Middle = Two
     };
 
     enum class ButtonState {
